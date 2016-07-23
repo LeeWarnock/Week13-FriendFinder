398a199a0bea6c7b3d269287f6074bfd9922e929
Lee Warnock <clwarnock@gmail.com>
2016-07-23 18:24:00 -0500

    adds html-routes.js

---
+++ app/routing/html-routes.js
@@ -0,0 +1,17 @@
+//Dependencies
+var path = require('path');
+
+//Routes
+
+module.exports = function(app){
+
+//GET req
+	app.get('/survey', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/survey.html'));
+	});
+
+//Sends user home if not match
+	app.use(function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+}
