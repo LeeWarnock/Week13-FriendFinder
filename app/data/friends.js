787445f17bd700f9682e37222cdc4026561d9b16
Lee Warnock <clwarnock@gmail.com>
2016-07-23 17:58:51 -0500

    adds friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,93 @@
+//Friends array. This is the inital set of friends.
+
+var friendsArray = [
+
+    {
+        "name": "Kimberly",
+        "photo": "http://2.bp.blogspot.com/-Gv4Q3GFrVW4/UH6uxIjkdsI/AAAAAAAAIzI/LnqvGNu6Zd4/s1600/kim2.jpg",
+        "scores": [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+
+    {
+        "name": "Sammy",
+        "photo": "http://www.dmg-entertainment.com/wp-content/uploads/2015/12/Samuel-L-Jackson1.jpg",
+        "scores": [
+            10,
+            10,
+            10,
+            10,
+            10,
+            10,
+            10,
+            10,
+            10,
+            10
+        ]
+    },
+
+    {
+        "name": "Danni",
+        "photo": "http://www.theweirdlings.com/wp-content/uploads/2013/04/Daenerys-attacks-Astapor.jpg",
+        "scores": [
+            9,
+            6,
+            6,
+            7,
+            8,
+            7,
+            6,
+            5,
+            3,
+            1
+        ]
+    },
+
+    {
+        "name": "Slenderman",
+        "photo": "http://media4.s-nbcnews.com/j/newscms/2014_23/491626/140606-slender-man-mn-735_297a98e4b78681fe7a932d8c5069f8db.nbcnews-fp-1200-800.jpg",
+        "scores": [
+            10,
+            10,
+            10,
+            10,
+            10,
+            10,
+            10,
+            10,
+            10,
+            10
+        ]
+    }
+
+    {
+        "name": "A Happy Dog",
+        "photo": "http://evansheline.com/wp-content/uploads/2012/04/happy-dog.jpg",
+        "scores": [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+        ]
+    }
+
+];
+
+//Exports the above array
+module.exports = friendsArray;
