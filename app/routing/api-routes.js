d13d6406605245b4557b2eff7c0edb68a22cbbe1
Lee Warnock <clwarnock@gmail.com>
2016-07-23 18:22:18 -0500

    adds api-routs.js

---
+++ app/routing/api-routes.js
@@ -0,0 +1,28 @@
+//Data sources  - linked to routes
+
+var friendsData 	= require('../data/friends.js');
+var path 			= require('path');
+
+//Routes
+
+module.exports = function(app){
+
+//GET req for friends json table tada
+
+	app.get('/api/friends', function(req, res){
+		res.json(friendsData);
+	});
+
+//POST req for survey results, adds jason data to array
+
+	app.post('/api/friends', function(req, res){
+
+//Pushes to friends data and finds match
+
+			friendsData.push(req.body);
+			res.json(true);
+
+
+	});
+
+}
